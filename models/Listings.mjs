4203d675ac028df812b96ee34b7e90c62485fa21
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-23 15:55:50 -0700

    Added Listings Schema

---
+++ models/Listings.mjs
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userListing = new mongoose.Schema({
+    userID: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 150
+    },
+    price: {
+        type: Number,
+        required: true,
+        minLength: 3
+    },
+    condition: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false,
+        maxLength: 250,
+    }
+ });
+
+userListing.index({title: 1})
+
+ export default new mongoose.model("Listing", userListing);
