3e8827f1d8206980cdaa5ab33fff5cbde6339dc5
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-23 18:30:52 -0700

    ListingController boilerplate

---
+++ controllers/ListingController.mjs
@@ -0,0 +1,7 @@
+import User from '../models/Users.mjs';
+
+const ListingController = {
+
+}
+
+export default ListingController;
--- routes/listings.js
+++ routes/listings.js
@@ -7,5 +7,7 @@ import express from 'express';
 // Initialize router as express Router()
 const router = express.Router();
 
+// Route to create new listing
+router.post('/', UserController.createUser);
 
 export default router;
