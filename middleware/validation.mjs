7f298a626899007f150d2fa96ecb6a53f192d600
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-26 02:17:06 -0700

    comments

--- middleware/validation.mjs
+++ middleware/validation.mjs
@@ -4,7 +4,7 @@ export const userValidationSchema = checkSchema({
     fname: {
         in: ['body'], // specify request location
         isString: true, // check value is string
-        trim: true, // remove whitespaces
+        trim: true, // remove leading and trailing whitespaces
         notEmpty: {
             errorMessage: 'First name is required'
         }
