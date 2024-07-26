1beb3d13207ab3258b8960063a1435dc4ff06ffa
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-26 03:10:22 -0700

    Added title validation

--- validators/ListingValidation.mjs
+++ validators/ListingValidation.mjs
@@ -4,5 +4,17 @@ export const listingValidation = checkSchema({
     userID: {
         isString: true,
         trim: true
+    },
+    title: {
+        in: ['body'],
+        isString: true,
+        trim: true,
+        isLength: {
+            options: {
+                min: 5,
+                max: 150
+            },
+            errorMessage: 'Title must be between 5 and 150 characters'
+        }
     }
 })
