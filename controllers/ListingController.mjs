5681f61929505861ac32fd06d4d1f5c195302888
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-26 03:31:38 -0700

    import ListingValidation to ListingController

--- controllers/ListingController.mjs
+++ controllers/ListingController.mjs
@@ -1,4 +1,6 @@
 import Listing from '../models/Listings.mjs';
+import { listingValidation, ListingValidation } from '../validators/ListingValidation.mjs'
+import { handleValidationErrors } from '../validators/HandleValidationErrors.mjs';
 
 const ListingController = {
     // Method to get ALL listing
@@ -12,28 +14,39 @@ const ListingController = {
     },
 
     // Method to create new listing
-    createListing: async (req, res) => {
-        const { userID, title, price, condition, description } = req.body;
-        try {
-            const newListing = await Listing.create({ userID, title, price, condition, description });
-            res.status(201).json({ newListing });
-        } catch (error) {
-            res.status(400).json({ error: error.message });
+    createListing: [
+        // input validation 
+        ListingValidation,
+        // handle validation error middleware
+        handleValidationErrors,
+
+        async (req, res) => {
+            const { userID, title, price, condition, description } = req.body;
+            try {
+                const newListing = await Listing.create({ userID, title, price, condition, description });
+                res.status(201).json({ newListing });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
         }
-    },
+    ],
 
     // Method to update listing
-    updateListing: async (req, res) => {
-        const { id } = req.params;
-        const { userID, title, price, condition, description } = req.body;
+    updateListing: [
+        listingValidation,
+        handleValidationErrors,
+        async (req, res) => {
+            const { id } = req.params;
+            const { userID, title, price, condition, description } = req.body;
 
-        try {
-            const updatedListing = await Listing.findByIdAndUpdate(id, { userID, title, price, condition, description });
-            res.json(updatedListing);
-        } catch (error) {
-            res.status(400).json({ error: error.message })
+            try {
+                const updatedListing = await Listing.findByIdAndUpdate(id, { userID, title, price, condition, description });
+                res.json(updatedListing);
+            } catch (error) {
+                res.status(400).json({ error: error.message })
+            }
         }
-    },
+    ],
 
     // Method to delete listing
     deleteListing: async (req, res) => {
--- controllers/UserController.mjs
+++ controllers/UserController.mjs
@@ -1,6 +1,6 @@
 import User from '../models/Users.mjs';
 import { v4 as uuidv4 } from 'uuid';
-import { userValidationSchema } from '../validators/UserValidation.mjs';
+import { userValidation } from '../validators/UserValidation.mjs';
 import { handleValidationErrors } from '../validators/HandleValidationErrors.mjs';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
@@ -8,99 +8,99 @@ import jwt from 'jsonwebtoken';
 const UserController = {
     // Method to create a new user
     createUser: [
-        // validation middleware
-        userValidationSchema,
+        // input validation
+        userValidation,
         // handle validation error middleware
         handleValidationErrors,
         async (req, res) => {
-        // Destructure body of request
-        const {fname, lname, username, email, password} = req.body;
+            // Destructure body of request
+            const { fname, lname, username, email, password } = req.body;
 
-        try {
-            // Check for existing username
-            let userExist = await User.findOne({username});
-            if(userExist)
-                return res.status(400).json({errors: [{msg: 'Username already exist'}]});
+            try {
+                // Check for existing username
+                let userExist = await User.findOne({ username });
+                if (userExist)
+                    return res.status(400).json({ errors: [{ msg: 'Username already exist' }] });
 
-            // Check for existing email
-            let emailExist = await User.findOne({email});
-            if(emailExist)
-                return res.status(400).json({errors: [{msg: 'Email already exist'}]});
+                // Check for existing email
+                let emailExist = await User.findOne({ email });
+                if (emailExist)
+                    return res.status(400).json({ errors: [{ msg: 'Email already exist' }] });
 
-            // Encrypt user password
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(password, salt);
-            
-            // Use mongoose method to create new user
-            const newUser = await User.create({
-                userID: uuidv4(), 
-                fname, 
-                lname, 
-                username,
-                password,
-                email,
-                password: hashedPassword,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                isActive: true
-            });
+                // Encrypt user password
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
 
-            // Create JWT payload
-            const payload = {
-                user: {
-                    id: newUser.userID,
-                    fname: newUser.fname,
-                    lname: newUser.lname
-                }
-            };
+                // Use mongoose method to create new user
+                const newUser = await User.create({
+                    userID: uuidv4(),
+                    fname,
+                    lname,
+                    username,
+                    password,
+                    email,
+                    password: hashedPassword,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    isActive: true
+                });
 
-            // Sign JWT and return it
-            jwt.sign(
-                payload,
-                process.env.jwtSecret,
-                {expiresIn: '1hr'},
-                (error, token) => {
-                    if(error) {
-                        console.error(error);
-                        return res.status(500).json({errors: [{msg: 'Server error'}]});
+                // Create JWT payload
+                const payload = {
+                    user: {
+                        id: newUser.userID,
+                        fname: newUser.fname,
+                        lname: newUser.lname
                     }
-                    res.status(201).json({token});
-                }
-            );
-        } catch (error) {
-            res.status(500).json({error: error.message});
-        }
-    }],
+                };
+
+                // Sign JWT and return it
+                jwt.sign(
+                    payload,
+                    process.env.jwtSecret,
+                    { expiresIn: '1hr' },
+                    (error, token) => {
+                        if (error) {
+                            console.error(error);
+                            return res.status(500).json({ errors: [{ msg: 'Server error' }] });
+                        }
+                        res.status(201).json({ token });
+                    }
+                );
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        }],
 
     // Method to update a user
     updateUser: async (req, res) => {
         // Deconstruct parameter of request
-        const {id} = req.params;
+        const { id } = req.params;
 
         // Deconstruct body of request
-        const {fname, lname, email} = req.body;
+        const { fname, lname, email } = req.body;
 
         try {
             // Mongoose method to find user by userID and then update data
-            const updatedUser = await User.findByIdAndUpdate(id, {fname, lname, email});
+            const updatedUser = await User.findByIdAndUpdate(id, { fname, lname, email });
             // Respond with newly updated user
             res.json(updatedUser);
         } catch (error) {
-            res.status(400).json({error: error.message})
+            res.status(400).json({ error: error.message })
         }
     },
 
     // Method to delete user
     deleteUser: async (req, res) => {
         // Deconstruct parameters of request
-        const {userID} = req.params;
+        const { userID } = req.params;
         try {
             // Mongoose method to find user by userID and delete user
             await User.findByIdAndDelete(userID);
             // Respond with message
-            res.json({message: 'User deleted successfully'})
+            res.json({ message: 'User deleted successfully' })
         } catch (error) {
-            res.status(400).json({error: error.message})
+            res.status(400).json({ error: error.message })
         }
     }
 }
