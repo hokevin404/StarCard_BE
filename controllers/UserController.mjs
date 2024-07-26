9e523103fc0b73ff791b5819ae319d476b43eae3
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-25 22:35:54 -0700

    Added comments

--- controllers/UserController.mjs
+++ controllers/UserController.mjs
@@ -4,10 +4,16 @@ import { v4 as uuidv4 } from 'uuid';
 const UserController = {
     // Method to create a new user
     createUser: async (req, res) => {
+        // Destructure body of request
         const {fname, lname, username, email} = req.body;
+        
+        // Generate unique userID
         const userID = uuidv4();
+
         try {
+            // Use mongoose method to create new user
             const newUser = await User.create({userID, fname, lname, username, email});
+            // respond with newly created user
             res.status(201).json({newUser});
         } catch (error) {
             res.status(400).json({error: error.message});
@@ -16,11 +22,16 @@ const UserController = {
 
     // Method to update a user
     updateUser: async (req, res) => {
+        // Deconstruct parameter of request
         const {id} = req.params;
+
+        // Deconstruct body of request
         const {fname, lname, email} = req.body;
 
         try {
+            // Mongoose method to find user by userID and then update data
             const updatedUser = await User.findByIdAndUpdate(id, {fname, lname, email});
+            // Respond with newly updated user
             res.json(updatedUser);
         } catch (error) {
             res.status(400).json({error: error.message})
@@ -29,9 +40,12 @@ const UserController = {
 
     // Method to delete user
     deleteUser: async (req, res) => {
+        // Deconstruct parameters of request
         const {userID} = req.params;
         try {
+            // Mongoose method to find user by userID and delete user
             await User.findByIdAndDelete(userID);
+            // Respond with message
             res.json({message: 'User deleted successfully'})
         } catch (error) {
             res.status(400).json({error: error.message})
